a6d84bdbf11ad2c7b25df46271fd55c656007758
marc <marc@wittke-web.de>
2022-01-26 15:41:08 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from "@angular/forms";
+import { MarvinJsModule } from "./marvin-js/marvin-js.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MarvinJsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/marvin-js/index.ts
@@ -0,0 +1,20 @@
+import {
+  ImageExporter,
+  MarvinJSUtilInstance,
+  MarvinPackage,
+  RenderParams,
+  RenderParamsSettings,
+  Sketch
+} from "./marvin-js";
+
+import { MarvinJsEditorComponent } from "./marvin-js-editor.component";
+
+export {
+  ImageExporter,
+  MarvinJsEditorComponent,
+  MarvinJSUtilInstance,
+  MarvinPackage,
+  RenderParams,
+  RenderParamsSettings,
+  Sketch
+};
